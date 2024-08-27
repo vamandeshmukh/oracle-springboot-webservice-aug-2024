c675a5916a8f9e5b6e5a547f29238ab49c4642da
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-27 10:33:15 +0530

    spring-boot-day-2

--- src/main/java/com/oracle/demo/controller/EmployeeController.java
+++ src/main/java/com/oracle/demo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.oracle.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oracle.demo.model.Employee;
@@ -17,6 +19,7 @@ import com.oracle.demo.service.IEmployeeService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 
 //	private IEmployeeService empService = new EmployeeService();
@@ -37,7 +40,8 @@ public class EmployeeController {
 	}
 
 //	http://localhost:8080/api/emp 
-	@PostMapping("emp")
+//	@PostMapping("emp")
+	@RequestMapping(method = RequestMethod.POST, path = "emp", consumes = "application/json", produces = "application/json")
 	public Employee addEmp(@RequestBody Employee employee) {
 		return empService.addEmployee(employee);
 	}
