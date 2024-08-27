08f8483b9867861db154f30159f39493cbb2e774
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-27 12:20:01 +0530

    spring-boot-day-2

--- src/main/java/com/oracle/demo/repository/EmployeeRepository.java
+++ src/main/java/com/oracle/demo/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.oracle.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,29 @@ import com.oracle.demo.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+//	for basic CRUD operations, no need to write any methods 
+	// INSERT 
+	// UPDATE 
+	// DELETE 
+	// SELECT 
+	// SELECT 
+
+//	for business specific operations only, methods need to declared here 
+	
+	// 2. use this method 
+	public abstract List<Employee> findByName(String name);
+
+//	1/ 
+	
+	// re-run your code with - 
+	// remove @Repository
+	// remove @Autowired 
+	// remove @Entity
+	// remove @Id 
+	// remove 
+//	public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+//	public interface EmployeeRepository extends JpaRepository<> {
+	
 
 }
